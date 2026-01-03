8f1a8819b5f43d12fdcdb6f63284b4530af8d794
Kazheru <herupkurni@gmail.com>
2026-01-03 07:08:09 +0700

    Tambah link Google Form pendaftaran di footer Tautan Cepat

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -77,6 +77,19 @@ const Footer: React.FC = () => {
       Buka Google Maps (Ulasan & Direction)
     </a>
   </li>
+  <li>
+  <a 
+    href="https://forms.gle/AHfTtxtUEkqvTrG36" 
+    target="_blank" 
+    rel="noopener noreferrer" 
+    className="hover:text-gray-300 transition-colors flex items-center space-x-2"
+  >
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+    </svg>
+    <span>Daftar / Antri Layanan</span>
+  </a>
+</li>
 </ul>
       </div>
 
