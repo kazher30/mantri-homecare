dd6e5197be28ac1ecb9018a0d159a01bdd8d88f0
Kazheru <herupkurni@gmail.com>
2026-01-19 11:21:37 +0700

    revisi lagi

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -47,7 +47,7 @@ return (
             </a>
 
             {/* Blogger */}
-            <a href="https://mantrihomecare.blogspot.com" target="_blank" rel="noopener noreferrer" className="w-10 h-10 flex items-auto justify-center">
+            <a href="https://mantrihomecare.blogspot.com" target="_blank" rel="noopener noreferrer" className="w-auto h-10 flex items-auto justify-center">
               <img 
                   src="/Blogger-Logo-2016-present.png"
                   alt="Mantri Home Care Blogger - Spesialis Rawat Luka & Sunat Modern Kediri"
