59a46272288534824a4bc0f0b596b5b9bf6ad026
kazheru <herupkurni@gmail.com>
2026-05-14 21:31:40 +0700

    ganti logo jasa kitan file Service.tsx

--- components/Services.tsx
+++ components/Services.tsx
@@ -10,7 +10,7 @@ const Services: React.FC = () => {
       waMessage: 'Halo Mantri Home Care, saya tertarik dengan layanan Khitan Modern (Super Ring/Sealer/Couter). Boleh konsultasi lebih lanjut?',
       icon: (
         <img 
-        src="/sunattopBW.png"  // atau /logo.svg kalau SVG
+        src="/SunatTop2.png"  // atau /logo.svg kalau SVG
         alt="Sunat Top Logo - Spesialis Rawat Luka & Sunat Modern Kediri"
         className="h-16 w-auto mr-4"  // ukuran logo di footer, sesuaikan kalau perlu
         />
