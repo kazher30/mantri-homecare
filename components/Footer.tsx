e7df953625fcce3e18679df4efd16f68ebd14f24
kazheru <herupkurni@gmail.com>
2026-05-14 20:02:56 +0700

    ganti facebook

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,7 @@ return (
               </a>
 
               {/* Facebook */}
-              <a href="https://www.facebook.com/mboh.raweruh.12" target="_blank" rel="noopener noreferrer" className="bg-blue-600 rounded-full w-10 h-10 flex items-center justify-center hover:bg-blue-700 transition">
+              <a href="https://www.facebook.com/share/1cN4shH4VT/" target="_blank" rel="noopener noreferrer" className="bg-blue-600 rounded-full w-10 h-10 flex items-center justify-center hover:bg-blue-700 transition">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="currentColor" viewBox="0 0 24 24">
                   <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385h-3.047v-3.47h3.047v-2.648c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953h-1.513c-1.491 0-1.956.925-1.956 1.874v2.255h3.328l-.532 3.47h-2.796v8.385c5.737-.9 10.125-5.864 10.125-11.854z"/>
                 </svg>
