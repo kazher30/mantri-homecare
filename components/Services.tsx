410fcacb03ffba48ea8e8ac60b44ed99de5fcd35
kazheru <herupkurni@gmail.com>
2026-05-15 07:48:33 +0700

    ganti h-60 layanan khitan

--- components/Services.tsx
+++ components/Services.tsx
@@ -12,7 +12,7 @@ const Services: React.FC = () => {
         <img 
           src="/layanan-sunat.png" 
           alt="Jasa Khitan Modern - Mantri Home Care Kediri"
-          className="h-40 w-full object-contain"   // Diperbesar
+          className="h-60 w-full object-contain"   // Diperbesar
         />
       ),
       color: 'bg-blue-50'
